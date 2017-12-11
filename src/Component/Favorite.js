98d009a3f2df84e06e0d06a77a8cb04abff5293b
Felix Fong <moongod101@hotmail.com>
2017-12-11 22:54:35 +0800

    fixed the favorite button didnt light up when user favorite the track already

--- src/Component/Favorite.js
+++ src/Component/Favorite.js
@@ -18,6 +18,18 @@ export default class Favorite extends React.Component {
         axios.post(getApiurl('api', `/track/favorite/${trackId}?`));
     }
 
+    async componentDidMount(){
+        const trackId = this.props.trackId;
+        const heart = this.refs.heart;
+        // Check if the user already favorite this track or not
+        const response = await axios.get(getApiurl('api', `/me?`));
+        if(response.data.fave.includes(trackId)) {
+            heart.classList.add('favorite');
+        }else{
+            heart.classList.remove('favorite');
+        }
+    }
+
     render() {
         return (
             <div ref="heart" className="fa fa-heart" onClick={this.favorite.bind(this)}></div>
