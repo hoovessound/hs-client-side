ed674c6a2be6b5167916c4f4355763d624d82f2b
Felix Fong <moongod101@hotmail.com>
2017-12-16 00:30:52 +0800

    re-ordering the elements

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import NavBar from './NavBar';
+import NavBar from '../Component/NavBar';
 export default class Layout extends React.Component {
     render() {
         return (
             <header>
-                <h1>HoovesSound</h1>
                 <NavBar/>
             </header>
         )
