fcb8645e2d43877007656b96e5c9fae0e8cee4d9
Felix Fong <felixfong@Felixs-MacBook-Air.local>
2017-12-15 20:12:33 +0800

    fixed redirect issue

--- src/Util/checkLogin.js
+++ src/Util/checkLogin.js
@@ -4,7 +4,7 @@ export default () => {
     const token = cookie.load('oauth-token');
     const currentUrl = window.location;
     if(!token){
-        if(process.env.NDOE_ENV === 'production'){
+        if(process.env.NODE_ENV === 'production'){
             window.location = `https://id.hoovessound.ml/login?service=hs_service_login&redirect=${currentUrl}`;
         }else{
             window.location = `http://id.hoovessound.app:3000/login?service=hs_service_login&redirect=${currentUrl}`;
