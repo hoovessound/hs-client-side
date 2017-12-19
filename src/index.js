8428db35582a0730410b18d8793902055b7122c9
Felix Fong <moongod101@hotmail.com>
2017-12-19 17:53:57 +0800

    add the unregister function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDom from 'react-dom';
 import Routers from './Router/Routers';
 import {Provider} from 'react-redux';
 import store from './Redux/store';
+import {unregister} from "./registerServiceWorker";
 
 const app = document.getElementById('app');
 ReactDom.render(<Provider store={store}>
     <Routers />
 </Provider>, app);
+unregister();
