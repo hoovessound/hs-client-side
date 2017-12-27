e927f6ee98bc7c3dd72d0b9c7a18be360094c86f
Felix Fong <moongod101@hotmail.com>
2017-12-27 18:18:32 +0800

    add a small developer notes

--- src/Util/checkLogin.js
+++ src/Util/checkLogin.js
@@ -1,6 +1,40 @@
 import cookie from 'react-cookies';
 import getUrl from './getApiUrl';
 
+/*
+
+Dear developers
+
+If you are working on this repo with out
+the local HoovesSound server environment running,
+please following the instruction in order
+to set you up
+
+INSTRUCTION:
+
+Create a HoovesSound account if you haven't
+Visit hoovessound.ml(The production HoovesSound site)
+Open up the developer tool
+Open the "Applications" tag
+Locate the "Cookies" tag
+Find the "oauth-token", and copy the value of it
+Un-comment the setUp function
+Paste your oauth-token at the setUp function
+
+WARNING:
+Do not commit your code with your oauth-token!!!
+If you do so, and already push your code to the repo,
+please change your password RIGHT A WAY NOW! DO IT NOW!!
+
+Felix
+
+*/
+
+// function setUp(oauthToken){
+//     cookie.save('oauth-token', oauthToken);
+// }
+// setUp('YOUR OAUTH TOKEN HERE');
+
 export default () => {
     const token = cookie.load('oauth-token');
     const currentUrl = window.location;
