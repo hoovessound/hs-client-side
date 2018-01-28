c0a14a0a469aefb7bfbddd35a5de01c80bac75bf
Felix Fong <moongod101@hotmail.com>
2018-01-28 20:56:38 +0800

    Update: redesign the upload page

--- src/Pages/Upload.js
+++ src/Pages/Upload.js
@@ -16,12 +16,22 @@ export default class Upload extends React.Component {
     audioFileOnChange(e){
         // Get the file name
         if(e.target.files[0]){
-            const fileName = e.target.files[0].name;
+            const file = e.target.files[0];
+            const fileName = file.name;
             const stripFileName = fileName.split('.')[0];
             const titleEl = this.refs.title;
             if(!titleEl.value){
                 titleEl.value = stripFileName;
             }
+        }
+    }
+
+    coverArtOnChange(e){
+        const file = e.target.files[0];
+        const fs = new FileReader();
+        fs.readAsDataURL(file);
+        fs.onload = () => {
+            this.refs.coverArt.style.backgroundImage = `url(${fs.result})`;
         }
     }
 
@@ -52,7 +62,7 @@ export default class Upload extends React.Component {
             // Redirect the user to the track page
             this.setState({
                 redirect: true,
-                redirectTo: `/${response.data.id}`,
+                redirectTo: `/track/${response.data.id}`,
             });
         }
     }
@@ -104,106 +114,105 @@ export default class Upload extends React.Component {
 
                 </div>
 
-                <form ref="uploadForm">
+                <form ref="uploadForm" className={'uploadForm'}>
 
                     {/*Media file and info*/}
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-3">
+                                <div
+                                    className="coverArt"
+                                    ref={'coverArt'}
+                                    style={{
+                                        position: 'relative',
+                                        width: '15em',
+                                        height: '15em',
+                                        border: '2px dashed #161616',
+                                        margin: '1em',
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                    }}
+                                >
+                                    <input type="file" name="image" id="image" onChange={this.coverArtOnChange.bind(this)}
+                                        style={{
+                                            opacity: '0',
+                                            width: '100%',
+                                            height: '100%',
+                                            cursor: 'pointer',
+                                        }}
+                                    />
+                                        <div className="placeholder"
+                                            style={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                position: 'absolute',
+                                                height: '100%',
+                                                width: '100%',
+                                                top: '0em',
+                                                left: '0em',
+                                                fontSize: '1.3em',
+                                                zIndex: '-1',
+                                                color: '#CCC',
+                                            }}
+                                        >
+                                            <div className="fa fa-camera"></div>
+                                        </div>
+                                </div>
+                            </div>
 
-                    <h1>Step 1: Select your audio file</h1>
+                            <div className="col-sm-9">
+                                <input type="text" className="title form-control" placeholder="Track title" ref={'title'} name={'title'} style={{margin: '0.5em 0em'}} />
+                                <textarea name="description" placeholder="Description" className="description form-control"
+                                    style={{
+                                        width: '100%',
+                                        height: '50%',
+                                        resize: 'none',
+                                        marginBottom: '0.5em',
+                                    }}
+                                ></textarea>
 
-                    <div
-                        id="audioFile"
-                        style={{
-                            position: 'relative',
-                            border: '3px solid #161616',
-                            width: '50em',
-                            height: '10em',
-                            margin: '0.5em auto'
-                        }}
-                    >
-
-                        <p style={{
-                            textAlign: 'center',
-                            fontSize: '1.2em',
-                            margin: '0.5em',
-                        }}>Drop your file here</p>
-
-                        <input
-                            ref="audioFile"
-                            type="file"
-                            name={'audio'}
-                            onChange={this.audioFileOnChange.bind(this)}
-                        style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        opacity: 0,
-                        width: '100%',
-                        height: '100%',
-                    }}
-                        />
+                                <div className="audioContainer"
+                                    style={{
+                                        width: '100%',
+                                        height: '20%',
+                                        border: '2px dashed #161616',
+                                        position: 'relative',
+                                    }}
+                                >
+                                    <input type="file" name="audio" id="audio" onChange={this.audioFileOnChange.bind(this)} style={{
+                                        opacity: '0',
+                                        width: '100%',
+                                        height: '100%',
+                                        cursor: 'pointer',
+                                    }} />
+                                    <div className="placeholder"
+                                        style={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            position: 'absolute',
+                                            height: '100%',
+                                            width: '100%',
+                                            top: '0em',
+                                            left: '0em',
+                                            color: '#CCC',
+                                            zIndex: '-1',
+                                        }}
+                                    >
+                                        <span>Audio track</span>
+                                    </div>
+                                </div>
+                                <a className="btn btn-outline-success uploadButton" onClick={this.uploadTrack.bind(this)}
+                                    style={{
+                                        display: 'block',
+                                        margin: '0.5em',
+                                    }}
+                                >Upload</a>
+                            </div>
+                        </div>
                     </div>
 
-                    <hr/>
-
-                    <h1>Step 2: Fill in the meta data</h1>
-
-                    <div id="title">
-                        <p>Title<span style={{color: 'red'}}>*</span></p>
-                        <input ref="title" type="text" name={'title'} />
-                    </div>
-
-                    <div id="description">
-                        <p>Description</p>
-                        <textarea ref="description" type="text" name={'description'} />
-                    </div>
-
-                    <hr/>
-
-                    <h1>Step 3: Choose a cover art</h1>
-
-                    <div
-                        id="audioFile"
-                        style={{
-                            position: 'relative',
-                            border: '3px solid #161616',
-                            width: '50em',
-                            height: '10em',
-                            margin: '0.5em auto'
-                        }}
-                    >
-
-                        <p style={{
-                            textAlign: 'center',
-                            fontSize: '1.2em',
-                            margin: '0.5em',
-                        }}>Drop your file here</p>
-
-                        <input
-                            ref="coverArtFile"
-                            type="file"
-                            name={'image'}
-                            onChange={this.audioFileOnChange.bind(this)}
-                            style={{
-                                position: 'absolute',
-                                top: 0,
-                                left: 0,
-                                opacity: 0,
-                                width: '100%',
-                                height: '100%',
-                            }}
-                        />
-                    </div>
-
-                    <hr/>
-
-                    <div
-                        className="btn btn-success"
-                        style={{
-                            cursor: 'pointer',
-                            marginTop: '0.5em',
-                        }}
-                        onClick={this.uploadTrack.bind(this)}
-                    >Upload</div>
                 </form>
                 {
                     (() => {
