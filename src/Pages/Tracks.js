aed031804b05fd45aa4a4772f03cff1767570d33
Felix Fong <moongod101@hotmail.com>
2018-02-08 18:16:45 +0800

    Fix: making extra API request

--- src/Pages/Tracks.js
+++ src/Pages/Tracks.js
@@ -8,41 +8,76 @@ export default class Tracks extends React.Component {
     constructor() {
         super();
         this.state = {
-            trackEl: []
+            trackEl: [],
+            offset: 0,
         }
     }
 
-    componentDidMount() {
-        axios.get(getApiurl('api', `/tracks?offset=0`))
-        .then(response => {
-            const tracks = response.data;
-
-            // Add some tracks to the local playlist
-            store.dispatch({
-                type: 'ADD_LOCAL_PLAYLIST',
-                payload: {
-                    tracks,
-                }
-            });
-
-            const trackEl = tracks.map(track => {
-                return (
+    async fetchMore(){
+        const url = getApiurl('api', `/tracks?offset=${this.state.offset}`);
+        const tracks = await axios.get(url);
+        const trackEl = this.state.trackEl;
+        if(!tracks.data.error){
+            tracks.data.map(track => {
+                return trackEl.push(
                     <TrackContainer key={track.id} track={track}/>
-                )
+                );
             });
+            console.log(trackEl)
             this.setState({
                 trackEl,
+                offset: (this.state.offset + tracks.data.length),
             });
-        })
-        .catch(error => {
-            console.log(error);
-        })
+        }else{
+            trackEl.push(
+                <div
+                    style={{
+                        color: 'red',
+                    }}
+                >ERROR: Can't not fetch more tracks, please try again later on</div>
+            )
+        }
+    }
+
+    async componentDidMount() {
+        const url = getApiurl('api', `/tracks?offset=${this.state.offset}`);
+        // const tracks = await axios.get(url);
+        // axios.get(getApiurl('api', `/tracks?offset=${this.state.offset}`))
+        // .then(response => {
+        //     const tracks = response.data;
+        //     // Add some tracks to the local playlist
+        //     store.dispatch({
+        //         type: 'ADD_LOCAL_PLAYLIST',
+        //         payload: {
+        //             tracks,
+        //         }
+        //     });
+        //
+        //     const trackEl = tracks.map(track => {
+        //         return (
+        //             <TrackContainer key={track.id} track={track}/>
+        //         )
+        //     });
+        //     this.setState({
+        //         trackEl,
+        //         offset: 10,
+        //     });
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        // })
     }
 
     render() {
         return (
             <div>
                 {this.state.trackEl}
+                <div className="btn btn-info"
+                    style={{
+                        cursor: 'pointer',
+                    }}
+                     onClick={this.fetchMore.bind(this)}
+                >More</div>
             </div>
         )
     }
